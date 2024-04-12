7225569dee0cdb82241f92902a3479473cd94588
Govind Rajewar <abhirajewar87@gmail.com>
2024-04-12 11:50:33 +0530

    26-03-2024: Introduction to React

---
+++ React/react-basics/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstSection from './components/FirstSection';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>First React App</h1>
+      <p>Hello, I am learning React.</p>
+
+      <FirstSection></FirstSection>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ React/react-basics/src/components/FirstSection.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function FirstSection() {
+  return (
+    <div className="FirstSection">
+      <h1>First Section App</h1>
+
+      <p>Hello, I am learning React.</p>
+
+    </div>
+  );
+}
+
+export default FirstSection;
