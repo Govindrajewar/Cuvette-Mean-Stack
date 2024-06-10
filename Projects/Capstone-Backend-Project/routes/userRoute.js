de57e9d2ad1e4548fc4d3b7924ceded9b8d0c480
Govind Rajewar <abhirajewar87@gmail.com>
2024-06-10 11:31:37 +0530

    Added encryption to store hashed password & compare hashed password

--- Projects/Capstone-Backend-Project/routes/userRoute.js
+++ Projects/Capstone-Backend-Project/routes/userRoute.js
@@ -1,82 +1,88 @@
-const express = require('express')
-
-const router = express.Router()
-const User = require('../model/User.js')
+const express = require("express");
+const router = express.Router();
+const User = require("../model/User.js");
+const bcrypt = require('bcrypt');
 
 // post, get, put, delete
 
-router.get('/', async (req, res) => {
-    const users = await User.find();
+router.get("/", async (req, res) => {
+  const users = await User.find();
 
-    res.json({
-        message: 'User route is working fine',
-        status: 'Working',
-        users: users
-    });
-})
-
+  res.json({
+    message: "User route is working fine",
+    status: "Working",
+    users: users,
+  });
+});
 
 // register
-router.post('/register', async (req, res) => {
+// TODO: Create a validation middleware
+router.post("/register", async (req, res) => {
 
-    try{
-        const { name, email, password } = req.body
+  try {
 
-        const newUser = new User({
-            name,
-            email,
-            password,
-        })
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email: email });
 
-        await newUser.save();
+    if (!existingUser) {
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-        res.status(201).json({
-            message: 'User registered successfully',
-            user: newUser
-        })
+      const newUser = new User({
+        name,
+        email,
+        password: hashedPassword,
+      });
 
-    } catch(error){
-        res.status(500).json({
-            status: 'ERROR',
-            message: error.message
-        })
+      await newUser.save();
+
+      res.status(201).json({
+        message: "User registered successfully",
+        user: newUser,
+      });
+    } else {
+      res.status(400).json({
+        status: "ERROR",
+        message: "User already exists",
+      });
     }
-
-})
-
+  } catch (error) {
+    res.status(500).json({
+      status: "ERROR",
+      message: error.message,
+    });
+  }
+});
 
 // login
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    try {
+  try {
+    const existingUser = await User.findOne({ email: email });
 
-        const existingUser = await User.findOne({ email: email });
-
-        if(existingUser){
-            if(existingUser.password === password){
-                res.status(200).json({
-                    message: 'Login is successful',
-                    user: existingUser.email,
-                });
-            }
-            else {
-                res.status(400).json({
-                    message: 'Invalid Password'
-                });
-            }
-        } else {
-            res.status(400).json({
-                message: 'User not found'
-            });
-        }
-
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal Server Error',
-            error,
+    if (existingUser) {
+    const isValidPassword = await bcrypt.compare(password, existingUser.password);
+      if (isValidPassword) {
+        res.status(200).json({
+          message: "Login is successful",
+          user: existingUser.email,
         });
+      } else {
+        res.status(400).json({
+          message: "Invalid Password",
+        });
+      }
+    } else {
+      res.status(400).json({
+        message: "User not found",
+      });
     }
-})
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+      error,
+    });
+  }
+});
 
-module.exports = router
+module.exports = router;
